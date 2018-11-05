bfbfd13d2d345dbf8f4ecfd2e089084904a34f9a
jérôme <jerome.piquet@atinternet.com>
2018-11-05 11:04:23 +0100

    better table name validation

--- validate.js
+++ validate.js
@@ -1,19 +1,13 @@
 'use strict'
 var ConfigError = require('./error')
+var regex = new RegExp('^[a-zA-Z0-9_.-]{3,255}$');
 
 var validate = function (options, field) {
-  if (options[field] === undefined) {
-    throw new ConfigError('InvalidConfig', field + ' is required')
+  if (!regex.test(options['field'] || '')) {
+    throw new ConfigError('InvalidConfig', field + 
+    '.tableName must follow AWS naming rules (3-255 length, and only the following characters: a-z, A-Z, 0-9, _-.)')
   }
-  if (options[field].tableName === undefined) {
-    throw new ConfigError('InvalidConfig', field + '.tableName is required')
-  }
-  if (typeof options[field].tableName !== 'string') {
-    throw new ConfigError('InvalidConfig', field + '.tableName should be string')
-  }
-  if (typeof options[field].tableName.length === 0) {
-    throw new ConfigError('InvalidConfig', field + '.tableName length should be more than 0')
-  }
+  return true
 }
 
 module.exports.config = function (options) {
